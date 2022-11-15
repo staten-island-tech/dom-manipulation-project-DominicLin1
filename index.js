02ccd1584632ebf6ce30d832b8f585f59dc2f6f5
DominicLin1 <dominiclin777@gmail.com>
2022-11-15 10:15:39 -0500

    commit 4

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const DOMSelectors =  {
-    button: document.getElementById("btn"),
-    text: document.querySelector("#text"),
-    empty: document.querySelector(".nothing")
+    button: document.getElementsByClassName("btn"),
+    title: document.getElementById("title"),
+    box : document.getElementsByClassName("text-box"),
+    input: document.querySelector(`#input`)
+
 };
 console.log(DOMSelectors);
+
+
+
+DOMSelectors.button.addEventListener("click", function (){
+  let input = DOMSelectors.input.value;
+  console.log(input);
+});
