95adc0448c35c8cc0a95130460a8b28857b7b382
DominicLin1 <dominiclin777@gmail.com>
2022-11-18 10:14:39 -0500

    commit 7

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const DOMSelectors =  {
   display: document.querySelectorAll("display"),
   artist: document.getElementById("artist"),
   url:document.getElementById("url"),
-  form:document.getElementsByClassName("text-box")
-
+  form:document.getElementById("form"),
+box: document.getElementById("box"),
+byebye: document.getElementsByClassName("remove btn")
 };
 console.log(DOMSelectors.submit);
 
 
 
-DOMSelectors.form.addEventListener("submit", function (){
+DOMSelectors.form.addEventListener("submit", function (event){
   let title = DOMSelectors.title.value;
   let artist = DOMSelectors.artist.value;
   let url = DOMSelectors.url.value;
@@ -25,4 +26,9 @@ console.log(artist);
 console.log(url); 
  
 console.log(`${title} ${artist} ${url}`)
+event.preventDefault();
 });
+
+DOMSelectors.byebye.addEventListener("submit", function remove(){
+
+})
