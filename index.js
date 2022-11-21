527d492b47d325b84f5efe61d01291ad2384a5db
DominicLin1 <dominiclin777@gmail.com>
2022-11-21 10:13:59 -0500

    commit 8

--- index.js
+++ index.js
@@ -29,6 +29,24 @@ console.log(`${title} ${artist} ${url}`)
 event.preventDefault();
 });
 
-DOMSelectors.byebye.addEventListener("submit", function remove(){
+DOMSelectors.byebye.addEventListener("submit", function remove(event){
+  
+});
 
-})
+function html(){
+
+  const card = {
+     title: DOMSelectors.title.value,
+artist: DOMSelectors.artist.value,
+ url: DOMSelectors.url.value
+}
+DOMSelectors.box.insertAdjacentHTML(
+  "afterend",
+`<div class="display-card">
+    <img class="display-img" src="https://www.gannett-cdn.com/presto/2019/08/08/USAT/dc1fe9c6-d585-42c4-9c69-e31cc46e4c94-ABBEY_ROAD.JPG?auto=webp&crop=1481,834,x0,y265&format=pjpg&width=1200"/>
+    <h2 class="display-artist">${card.artist}</h2>
+    <h3 class="display-album">${card.title}</h3>
+    <h3 class="display-url">${card.url}</h3>
+     <button class="remove btn">Remove Album</button>
+</div>`)
+};
